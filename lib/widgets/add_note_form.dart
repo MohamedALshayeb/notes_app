1184378f21cb2404ff0254d6d41d5436cc93df64
MohamedALshayeb <ms7815061@gmail.com>
2024-12-26 11:38:49 +0200

    delete note done

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -54,7 +54,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                     formKey.currentState!.save();
                     var currentdate = DateTime.now();
                     var formatCurrentDate =
-                        DateFormat('dd-mm-yy').format(currentdate);
+                        DateFormat('dd-mm-yyyy').format(currentdate);
                     var noteModel = NoteModel(
                         title: title!,
                         subTitle: subTitle!,
